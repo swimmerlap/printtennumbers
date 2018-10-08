81a6a2ef1b20e29975974cc746be8fa7cd98ca33
GBTC406002UR <GBTC406002UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-08 11:09:31 -0400

    second commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+        public class Main {
+
+            public static void main(String[] args) {
+
+
+                int num=10;
+                for( num=10; num>=1; num--)
+                {
+                    System.out.println(num);
+                }
+                // write your code here
+            }
+
+            // write your code here
+    }
